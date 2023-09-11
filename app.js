615b5d4a0f120e3fb44785cd2aa4584d2a8abbb3
SuayMack <pry_chu@hotmail.com>
2023-09-11 20:49:29 -0300

    feat: servidor express

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express'
+
+/**
+ * instância do Express
+ * inicialização do que foi importado
+ */
+const app = express()
+/**
+ * varável de alocação de porta
+ */
+const port = process.env.PORT | 3000
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
+
+//*midware* usar sempre depois da const app e antes das rotas
+// Usar depois do POST
+app.use(express.json())
