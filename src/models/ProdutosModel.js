dffb789c1993f99cc799c6e59f9f45ecd226c098
Jhosh-Christopher <shuanbrj@gmail.com>
2023-09-16 18:24:39 -0300

    feat: Criação do arquivo ProdutosModel.js

--- src/models/ProdutosModel.js
+++ src/models/ProdutosModel.js
@@ -1,1 +1,18 @@
-export default ProdutosModel
+import mongoose from 'mongoose'
+
+/**
+ * Objeto modelo para usuários
+ */
+
+const Produtos = mongoose.model('Produtos', {    
+
+    categoria : String,
+    tipo : String,
+    descricao : String,
+    valor: String
+})
+
+
+
+
+export default Produtos
