69e159af26028461ae7370d9893d6b1214acabb6
Jhosh-Christopher <shuanbrj@gmail.com>
2023-09-18 15:46:10 -0300

    feat: Validações do Feedback

--- src/services/ValidacoesFeedbacks.js
+++ src/services/ValidacoesFeedbacks.js
@@ -1,1 +1,22 @@
+class ValidacoesFeedbacks {
+
+
+    static validaDescricao(descricao) {
+        if (descricao.length >= 20 && descricao.length <= 350) {
+            return true
+        }
+        else {
+            throw new Error("A descrição deve conter no mínimo 20 e no máximo 350 caracteres")
+        }
+    }
+
+    static validaFeedbacks(descricao) {
+        try {
+            ValidacoesFeedbacks.validaFeedbacks(descricao)
+        } 
+        catch(error){
+            throw error
+        }
+    }
+}
 export default ValidacoesFeedbacks;
