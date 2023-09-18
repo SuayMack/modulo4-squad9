deb63641f372b36d96300f9f19e80ef1784b546a
SuayMack <pry_chu@hotmail.com>
2023-09-18 15:45:01 -0300

    feat: criação da validação de produtos

--- src/services/ValidacoesProdutos.js
+++ src/services/ValidacoesProdutos.js
@@ -1,1 +1,33 @@
-export default ValidacoesProdutos;
+class ValidacoesProdutos {
+  static validaNome(nome) {
+    const nomes = ["Software", "APP", "Site"]
+
+    if (nome.includes(nomes)) {
+      return true
+    }
+    else {
+      throw new Error("Produto inválido, o produto deve ser Software, APP ou Site")
+    }
+  }
+
+  static validaDescricao(descricao) {
+  
+    if (descricao.length >= 20 && descricao.length <= 350) {
+      return true
+    }
+    else {
+      throw new Error("A descrição deve conter no mínimo 20 e no máximo 350 caracteres")
+    }
+    
+  }
+  static async validaProduto(nome, descricao) {
+    try {
+      ValidacoesCliente.validaNome(nome)
+      ValidacoesCliente.validaDescricao(descricao)
+    } catch (error) {
+      throw error
+    }
+  }
+}
+
+export default ValidacoesProdutos
