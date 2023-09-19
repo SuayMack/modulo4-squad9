25a9012003a5590156b337c2d537830fa953d510
SuayMack <pry_chu@hotmail.com>
2023-09-19 16:32:07 -0300

    feat: criação de atributos

--- src/models/PedidosModel.js
+++ src/models/PedidosModel.js
@@ -2,7 +2,8 @@ import { Double } from "bson"
 import mongoose from "mongoose"
 
 const Pedidos = mongoose.model('Pedido', {
-
+    cliente: String,
+    produto: String,
     descricao: String
 
 })
