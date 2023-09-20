bbc051bacacf9073ce21f41d1d78b9335c06c637
Jhosh-Christopher <shuanbrj@gmail.com>
2023-09-20 16:12:42 -0300

    fix: Corrigindo bug na validação de email

--- src/Repository/ClientesRepository.js
+++ src/Repository/ClientesRepository.js
@@ -17,6 +17,11 @@ class ClientesRepository {
         return response
     }
 
+    static async buscarClientePorEmail(email){
+        const response = await RepositoryGeneral.buscarPorChave(Clientes, 'email', email)
+        return response
+    }
+    
     static async atualizaClientePorId(id, cliente){
         const response = await RepositoryGeneral.atualizaPorId(Clientes, id, cliente)
         return response
